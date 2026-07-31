7eb3866298044e3263fe4355b3dfa39375ab2f71
ChampionAsh5357 <ash@ashwork.net>
2026-07-31 09:10:40 -0400

    fix(common,client): Prevent server resource leak in reload listener

--- src/client/java/net/ashwork/mc/dimensions/client/AshsDimensionsClient.java
+++ src/client/java/net/ashwork/mc/dimensions/client/AshsDimensionsClient.java
@@ -6,17 +6,15 @@ import net.ashwork.mc.dimensions.client.model.ExpandedItemModelGeneratorLoader;
 import net.ashwork.mc.dimensions.client.neoext.ClientExtensions;
 import net.ashwork.mc.dimensions.client.neoext.ModelExtensions;
 import net.ashwork.mc.dimensions.client.network.ClientPayloadRegistrar;
-import net.ashwork.mc.dimensions.tags.DimensionItemTags;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.ashwork.mc.dimensions.resources.AdvancementRequirementsFlipper;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.ModelEvent;
-import net.neoforged.neoforge.client.event.RenderHandEvent;
 import net.neoforged.neoforge.common.NeoForge;
+
+import java.util.Collections;
 
 @Mod(value = AshsDimensions.ID, dist = Dist.CLIENT)
 public class AshsDimensionsClient {
@@ -27,9 +25,14 @@ public class AshsDimensionsClient {
         EntityRenderers.register(modBus);
         ModelExtensions.setup();
         modBus.addListener(AshsDimensionsClient::registerLoaders);
+        NeoForge.EVENT_BUS.addListener(AshsDimensionsClient::clearSyncedListeners);
     }
 
     private static void registerLoaders(ModelEvent.RegisterLoaders event) {
         event.register(ExpandedItemModelGeneratorLoader.ID, ExpandedItemModelGeneratorLoader.INSTANCE);
+    }
+
+    private static void clearSyncedListeners(ClientPlayerNetworkEvent.LoggingOut event) {
+        AdvancementRequirementsFlipper.INSTANCE.setRequirements(Collections.emptyList());
     }
 }
--- src/common/java/net/ashwork/mc/dimensions/resources/DimensionResources.java
+++ src/common/java/net/ashwork/mc/dimensions/resources/DimensionResources.java
@@ -3,13 +3,17 @@ package net.ashwork.mc.dimensions.resources;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 import net.neoforged.neoforge.event.OnDatapackSyncEvent;
+import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
+
+import java.util.Collections;
 
 public interface DimensionResources {
 
     static void setup() {
         NeoForge.EVENT_BUS.addListener(DimensionResources::registerReloadListeners);
         NeoForge.EVENT_BUS.addListener(DimensionResources::sendOnReload);
+        NeoForge.EVENT_BUS.addListener(DimensionResources::clearServerListeners);
     }
 
     static void registerReloadListeners(AddServerReloadListenersEvent event) {
@@ -19,5 +23,9 @@ public interface DimensionResources {
     static void sendOnReload(OnDatapackSyncEvent event) {
         var payload = AdvancementRequirementsFlipper.INSTANCE.createPayload();
         event.getRelevantPlayers().forEach(player -> PacketDistributor.sendToPlayer(player, payload));
+    }
+
+    static void clearServerListeners(ServerStoppedEvent event) {
+        AdvancementRequirementsFlipper.INSTANCE.setRequirements(Collections.emptyList());
     }
 }
